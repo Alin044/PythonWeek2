31fd6d3cd9004ba2c92e997e6074e4d25cf4a946
Alin044 <alinalin03030@gmail.com>
2026-02-18 15:38:14 +0000

    classes

--- staticMethods/main.py
+++ staticMethods/main.py
@@ -1,2 +1,25 @@
 #Static Methods
 
+class Employee:
+    def __init__(self, name, position):
+        self.name = name
+        self.position = position
+
+    def get_info(self):
+        return f"{self.name} is a {self.position}"
+
+    @staticmethod
+    def is_valid_position(position):
+        valid_positions = ["Manager", "Cashier", "Cook", "Janitor"]
+        return position in valid_positions
+
+pos = Employee.is_valid_position("Manager")
+print(pos)
+
+employee1 = Employee("Radu", "Manager")
+employee2 = Employee("Mihai", "Cook")
+employee3 = Employee("Ioana", "Janitor")
+
+print(employee1.get_info())
+print(employee2.get_info())
+print(employee3.get_info())
