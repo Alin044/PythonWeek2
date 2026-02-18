b9fe6597b09d2eed548da9acf223af7767152b43
Alin044 <alinalin03030@gmail.com>
2026-02-18 15:48:40 +0000

    classes

--- staticMethods/main.py
+++ staticMethods/main.py
@@ -1,3 +1,34 @@
+#Class methods
+
+class Student:
+
+    count = 0
+    total_gpa = 0
+
+    def __init__(self, name, gpa):
+        self.name = name
+        self.gpa = gpa
+        Student.count += 1
+        Student.total_gpa += gpa
+
+    def get_info(self):
+        return f"{self.name} has a GPA of {self.gpa}" #this is an instance method
+
+    @classmethod
+    def get_count(cls):
+        return f"Total number of students: {cls.count}"
+
+    @classmethod
+    def get_average_gpa(cls):
+        if cls.count == 0:
+            return 0
+        else:
+            return f"Average class gpa :  {cls.total_gpa / cls.count}"
+
+student1 = Student("Radu", 3.5)
+student2 = Student("Mihai", 4.0)
+print(Student.get_count())
+
 #Static Methods
 
 class Employee:
@@ -12,6 +43,7 @@ class Employee:
     def is_valid_position(position):
         valid_positions = ["Manager", "Cashier", "Cook", "Janitor"]
         return position in valid_positions
+
 
 pos = Employee.is_valid_position("Manager")
 print(pos)
